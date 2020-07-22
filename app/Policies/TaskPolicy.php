95d9de05e474e393fd6634bc8199f869d45625ee
Mikhail <kapustinomm@gmail.com>
2020-07-22 18:15:25 +0300

    Task policy

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+    
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return mixed
+     */
+    public function update(User $user, Task $task)
+    {
+        return $user->id === $task->user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return mixed
+     */
+    public function delete(User $user, Task $task)
+    {
+        return $user->id === $task->user->id && $task->isComplete;
+    }
+}
